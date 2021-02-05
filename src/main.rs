76ec217302ce1584c3b2c4d32513641296818f63
shabbamm <wbryan@protonmail.com>
2021-02-05 11:50:12 -0800

    formated println

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod population;
 
 fn main() {
     let mut paris = population::Settlement::new();
-    println!("{:?}", paris);
+    println!("{:#?}", paris);
     paris.add_community();
-    println!("{:?}", paris);
+    println!("{:#?}", paris);
     paris.society[0].add_size(23);
-    println!("{:?}", paris);
+    println!("{:#?}", paris);
 }
