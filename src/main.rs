e9b294a20b256493ae32bb99108eb8876c7e9ddf
shabbam <wbryan@protonmail.com>
2021-03-05 18:10:00 -0800

    created the GameState struct

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,31 @@ mod resources;
 mod utility;
 
 use geography::*;
+use population::Community;
 use std::collections::HashMap;
 
-fn main() {}
+fn main() {
+    let game_state = GameState::new();
+    println!("{:#?}", game_state);
+}
 
+#[derive(Debug)]
 struct GameState {
     world: HashMap<u32, World>,
     contintents: HashMap<u32, Continent>,
     regions: HashMap<u32, Region>,
-    settlement: HashMap<u32, Settlement>,
+    settlements: HashMap<u32, Settlement>,
+    communities: HashMap<u32, Community>,
 }
 
-impl GameState {}
+impl GameState {
+    fn new() -> GameState {
+        GameState {
+            world: HashMap::new(),
+            contintents: HashMap::new(),
+            regions: HashMap::new(),
+            settlements: HashMap::new(),
+            communities: HashMap::new(),
+        }
+    }
+}
