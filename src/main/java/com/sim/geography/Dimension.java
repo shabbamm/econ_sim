e0eabce337fab755b00c848add825ac29109accf
aycippo <aylah.cippoletti@icloud.com>
2021-06-06 11:28:32 -0700

    removed some eclipse files that were made somehow, and fixed the public field issue w/ geography

--- src/main/java/com/sim/geography/Dimension.java
+++ src/main/java/com/sim/geography/Dimension.java
@@ -1,8 +1,8 @@
 package com.sim.geography;
 
 public class Dimension {
-    int id;
-    String name;
+    public int id;
+    public String name;
 
     public Dimension(int id, String name) {
         System.out.print("Dimension " + id + " initializing...");
